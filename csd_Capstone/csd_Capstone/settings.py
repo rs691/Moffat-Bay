67ed0482401eb09bba0c8e8bd8df86c72ba1a3cc
rs691 <robstewart@my365.bellevue.edu>
2025-03-09 19:10:58 -0500

    Final commit part 3

--- csd_Capstone/csd_Capstone/settings.py
+++ csd_Capstone/csd_Capstone/settings.py
@@ -81,8 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',  # Use the MySQL backend
         'NAME': 'moffat_bay_holy_db',                 # Name of your database
         'USER': 'root',                       # MySQL username
-        # Must add own password to connect to database
-        'PASSWORD': '',             # MySQL password (leave blank if none)
+        'PASSWORD': '!',             # MySQL password (leave blank if none)
         'HOST': '127.0.0.1',                  # Database server (localhost for local dev)
                          
     }
